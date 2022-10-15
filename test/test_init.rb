fea96d730d8b32ca476ed5260bfd284ba86cab4c
Nathan Ladd <ntl@users.noreply.github.com>
2022-10-15 12:44:21 -0500

    Initial commit

---
+++ test/test_init.rb
@@ -0,0 +1,15 @@
+ENV['BOOTSTRAP'] ||= 'on'
+
+require_relative '../init'
+
+if ENV['BOOTSTRAP'] == 'on'
+  require 'test_bench/bootstrap'; TestBench::Bootstrap.activate
+else
+  require 'test_bench'; TestBench.activate
+end
+
+require 'test_bench/random/controls'
+
+include TestBench
+
+Controls = TestBench::Random::Controls rescue nil
